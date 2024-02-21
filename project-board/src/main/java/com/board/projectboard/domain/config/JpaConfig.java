febf2c4174fb8cac5ac25cf2dc2b988c16b85e54
ddongjunn <ddongjunn@gmail.com>
2024-02-21 17:46:16 +0900

    #10 jpa 연결 테스트 진행
    #10 jpa 연결 테스트

---
+++ project-board/src/main/java/com/board/projectboard/domain/config/JpaConfig.java
@@ -0,0 +1,19 @@
+package com.board.projectboard.domain.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.util.Optional;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+
+    @Bean
+    public AuditorAware<String> auditorAware(){
+        return () -> Optional.of("djlee"); // TODO : 스프링 시큐리티로 인증 기능 추가시 수정 예정
+    }
+
+}
---
+++ project-board/src/test/java/com/board/projectboard/repository/ArticleRepositoryTest.java
@@ -0,0 +1,41 @@
+package com.board.projectboard.repository;
+
+import com.board.projectboard.domain.Article;
+import com.board.projectboard.domain.config.JpaConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("JPA 연결 테스트")
+@Import(JpaConfig.class)
+@DataJpaTest
+class JpaRepositoryTest {
+
+    private final ArticleRepository articleRepository;
+
+    private final ArticleCommentRepository articleCommentRepository;
+
+    JpaRepositoryTest(@Autowired ArticleRepository articleRepository, @Autowired ArticleCommentRepository articleCommentRepository) {
+        this.articleRepository = articleRepository;
+        this.articleCommentRepository = articleCommentRepository;
+    }
+
+    @Test
+    void givenTestData_whenSelecting_thenWorksFine(){
+        //given
+
+        //when
+        List<Article> articles = articleRepository.findAll();
+
+        //then
+        assertThat(articles)
+                .isNotNull()
+                .hasSize(300);
+    }
+}
