7bbb9a38009890437ff8b43ec0101331f80a0a91
ddongjunn <ddongjunn@gmail.com>
2024-02-22 14:49:23 +0900

    #18 - ArticleControllerTest Modle 검증 다른 메소드로 수정
    ArticleControllerTest Modle 검증 메소드 attributeExists로 수정

--- project-board/src/test/java/com/board/projectboard/controller/ArticleControllerTest.java
+++ project-board/src/test/java/com/board/projectboard/controller/ArticleControllerTest.java
@@ -21,7 +21,6 @@ class ArticleControllerTest {
         this.mvc = mvc;
     }
 
-    @Disabled("구현 중")
     @DisplayName("[view][GET] 게시글 리스트 (게시판) 페이지 - 정상 호출")
     @Test
     void givenNothing_whenRequestingArticlesView_thenReturnsArticlesView() throws Exception {
@@ -30,9 +29,9 @@ class ArticleControllerTest {
         //When & Then
         mvc.perform(get("/articles"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/index"))
-                .andExpect(model().attributeDoesNotExist("articles"));
+                .andExpect(model().attributeExists("articles"));
     }
 
     @Disabled("구현 중")
@@ -46,8 +45,8 @@ class ArticleControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/detail"))
-                .andExpect(model().attributeDoesNotExist("article"))
-                .andExpect(model().attributeDoesNotExist("articleComments"));
+                .andExpect(model().attributeExists("article"))
+                .andExpect(model().attributeExists("articleComments"));
     }
 
     @Disabled("구현 중")
@@ -60,7 +59,7 @@ class ArticleControllerTest {
         mvc.perform(get("/articles/search"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.TEXT_HTML))
-                .andExpect(model().attributeDoesNotExist("articles/search"));
+                .andExpect(model().attributeExists("articles/search"));
     }
 
     @Disabled("구현 중")
@@ -73,6 +72,6 @@ class ArticleControllerTest {
         mvc.perform(get("/articles/search-hashtag"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.TEXT_HTML))
-                .andExpect(model().attributeDoesNotExist("articles/search-hashtag"));
+                .andExpect(model().attributeExists("articles/search-hashtag"));
     }
 }
