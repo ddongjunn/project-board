494ebfc1c17834c41850bd6a2fcb4f3645857550
ddongjunn <ddongjunn@gmail.com>
2024-02-22 15:01:53 +0900

    #18 - 게시판 뷰 기본 작성
    컨트롤러 핸들러 메소드로 테스트를 통과할 수 있또록 처리.
    테스트에서는 media type 검사에서 부가적인 옵션이 들어갈 수 있음을 확인했기 떄문에 테스트 메소드 수정

--- project-board/src/main/java/com/board/projectboard/controller/ArticleController.java
+++ project-board/src/main/java/com/board/projectboard/controller/ArticleController.java
@@ -1,7 +1,13 @@
 package com.board.projectboard.controller;
 
+import com.board.projectboard.domain.Article;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * /articles
@@ -13,5 +19,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class ArticleController {
 
+    @GetMapping
+    public String articles(ModelMap map){
+        map.addAttribute("articles", List.of());
+        return "articles/index";
+    }
 
 }
